4c60d7be08dd40a119e32bb60674875a8228e796
gabylan12 <gabriel.radikal@gmail.com>
2018-04-28 21:02:49 -0300

    add maven version in web app

--- wildfly-samples/clustering/http/src/main/java/uy/com/gabylan/MyServlet.java
+++ wildfly-samples/clustering/http/src/main/java/uy/com/gabylan/MyServlet.java
@@ -1,7 +1,11 @@
 package uy.com.gabylan;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
@@ -10,17 +14,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.infinispan.manager.CacheContainer;
 
 @WebServlet(value = "/MyServlet")
 public class MyServlet extends HttpServlet {
 
     @Resource(lookup = "java:jboss/infinispan/container/infinispan_cache")
-    static CacheContainer cache;
+    CacheContainer cache;
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        response.getWriter().println(getClass().getPackage().getImplementationVersion());
         response.getWriter().println(cache.getCache().get("param1"));
         cache.getCache().put("param1",request.getParameter("param1"));
     }
